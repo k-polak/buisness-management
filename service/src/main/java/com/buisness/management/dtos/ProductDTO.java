2513f732d633d4bdec801de4ab7a807e7573eca8
ksiezyk <Dupa1234>
2021-01-05 11:55:03 +0100

    Fix createProduct

--- service/src/main/java/com/buisness/management/dtos/ProductDTO.java
+++ service/src/main/java/com/buisness/management/dtos/ProductDTO.java
@@ -3,11 +3,15 @@ package com.buisness.management.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder(builderClassName = "ProductBuilder", toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class ProductDTO {
     Integer id;
--- service/src/main/java/com/buisness/management/products/boundary/ProductsResource.java
+++ service/src/main/java/com/buisness/management/products/boundary/ProductsResource.java
@@ -4,9 +4,8 @@ import com.buisness.management.dtos.ProductDTO;
 import com.buisness.management.products.control.ProductsController;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/products")
@@ -15,11 +14,14 @@ public class ProductsResource {
     ProductsController productsController;
 
     @GET
+    @Produces({MediaType.APPLICATION_JSON})
     public List<ProductDTO> getAllProducts(){
         return productsController.getAllProducts();
     }
 
     @POST
+    @Path("create")
+    @Consumes({MediaType.APPLICATION_JSON})
     public void createProduct(ProductDTO productDTO){
         productsController.createProduct(productDTO);
     }
