62566895e0447e166ee887f977bea1378fdb27b1
k-polak <44200232+k-polak@users.noreply.github.com>
2021-01-10 23:25:40 +0100

    Add basic view

---
+++ view/src/Customers.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Panel from 'react-bootstrap/lib/Panel'
+import Button from 'react-bootstrap/lib/Button'
+import CustomerDetails from './CustomerDetails'
+import axios from 'axios'
+
+export default class Customers extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedCustomer: 1
+    }
+  }
+
+  //function which is called the first time the component loads
+  componentDidMount() {
+    this.getCustomerData();
+  }
+
+  //Function to get the Customer Data from json
+  getCustomerData() {
+    axios.get('/service-1.0-SNAPSHOT/clients').then(response => {
+      this.setState({customerList: response})
+    })
+  };
+
+  render() {
+    if (!this.state.customerList)
+      return (<p>Loading data</p>)
+    return (<div className="addmargin">
+      <div className="col-md-3">
+        {
+
+          this.state.customerList.data.map(customer => <Panel bsStyle="info" key={customer.id} className="centeralign">
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">{customer.firstName}</Panel.Title>
+            </Panel.Heading>
+            <Panel.Body>
+              <p>{customer.lastName}</p>
+              <p>{customer.city}</p>
+              <Button bsStyle="info" onClick={() => this.setState({selectedCustomer: customer.id})}>
+
+                Click to View Details
+
+              </Button>
+
+            </Panel.Body>
+          </Panel>)
+        }
+      </div>
+      <div className="col-md-6">
+
+      </div>
+    </div>)
+  }
+
+}
